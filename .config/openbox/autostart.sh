d2d8391170e4e93718617c810cc96183acdd6f6f
gautamits <gautamamits95@gmail.com>
2018-04-25 19:10:54 +0530

    from mountblue PC

--- .config/openbox/autostart.sh
+++ .config/openbox/autostart.sh
@@ -3,10 +3,12 @@ nitrogen --restore &
 compton &
 slack &
 franz &
-xinput set-prop 12 292 1 &
+xinput set-prop 12 293 1 &
 plank &
 redshift &
 xset b off &
 clipit &
 nm-applet &
 volti &
+sleep 30s && curl -s portal.91springboard.com/login -c cookie.txt -d "username=amit.gautam@mountblue.io&password=test123" &
+sleep 20s && conky -c ~/.config/conky/sidepanes/sidepanes.conkyrc &
