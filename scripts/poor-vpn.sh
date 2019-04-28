3643c8c795efc442a2f4c10ecae4c5e5175ea9ea
gautamits <gautamamits95@gmail.com>
2019-04-28 21:47:19 +0530

    changed polybar, removed battery discharge animation

--- scripts/poor-vpn.sh
+++ scripts/poor-vpn.sh
@@ -7,7 +7,9 @@ servers=$(zenity --list --checklist --title="choose the servers to shuttl" \
 	--column "Pick" \
 	--column "domain" \
 	--column "ip" \
+	--width=500 \
+	--height=500 \
 	--print-column=3 \
 	--hide-column=3 \
 	$query )
-sshuttle --dns -e "ssh -i ~/workspace/amit.key" -r amit@sso.magicpin.in $servers
+sshuttle  --dns -e "ssh -i ~/workspace/amit.key" -r amit@sso.magicpin.in $servers
